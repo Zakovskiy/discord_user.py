88b7547a2bd1904ca5fb69757f2d0d8a09e33711
zak <60068872+Zakovskiy@users.noreply.github.com>
2021-01-20 16:42:45 +0300

    Update example.py

--- example.py
+++ example.py
@@ -7,10 +7,10 @@ DS = discorduser.Client("your email", "your password");
 servers_list = DS.list_guilds();
 
 # get server channels
-server_channels_list  = DS.guild_channels(servers_list[4]["id"]);
+server_channels_list  = DS.guild_channels(servers_list[0]["id"]);
 
 # saving select channel_id
-channel_id = server_channels_list[2]["id"];
+channel_id = server_channels_list[0]["id"];
 
 # send message in the channel
 DS.send_message("hello everyone", channel_id);
